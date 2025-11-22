8cf7285481d2c63bbac7d2cdf368fa0ee62154ec
LastRefund <rajden357@gmail.com>
2025-11-22 20:41:32 +0300

    Add files via upload

---
+++ Genshin dumper/dumper.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <Windows.h>
+
+// Function to start the IL2CPP dumper
+DWORD WINAPI Run(LPVOID lpParam);
---
+++ Genshin dumper/pch.h
@@ -0,0 +1,24 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "framework.h"
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <set>
+#include <string>
+#include <thread>
+#include <vector>
+#include <Psapi.h>
+#include <TlHelp32.h>
+#include <Windows.h>
+
+#endif
